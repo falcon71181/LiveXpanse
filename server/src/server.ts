96d41f4029b450e383e331d47aa5da913e1b968b
falcon71181 <abhaythakur71181@gmail.com>
2024-02-22 22:43:42 +0530

    Health Check API Route

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,11 @@ const SERVER_PORT = process.env.SERVER_PORT || 3333;
 
 const app = express();
 
+// Health Check API Route
+app.get("/health", (_req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on : ${SERVER_PORT}`);
 });
