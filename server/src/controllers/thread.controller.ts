bbce75153e9ab07f51cd9173ec98b6008d073b53
falcon71181 <abhaythakur71181@gmail.com>
2024-02-27 20:22:07 +0530

    Middleware used in Create thread
    Middleware isAuth is used . isAuth username variable name changed.

--- server/src/controllers/thread.controller.ts
+++ server/src/controllers/thread.controller.ts
@@ -1,44 +1,29 @@
 import { creatTable } from "../database/threads";
 import { pool } from "../database/db";
-import jwt from "jsonwebtoken";
 import type { RequestHandler, Request, Response } from "express";
 import type { QueryResult } from "pg";
-import type { JwtPayload } from "jsonwebtoken";
 
 // create thread
 const createThread: RequestHandler = async (req: Request, res: Response) => {
-  const JWT_SECRET = process.env.JWT_SECRET || "secret";
-
   try {
     let { title, message } = req.body;
-    const { authorization } = req.headers;
 
-    if (!authorization) {
-      const error = "Auth token not provided";
-      return res.status(400).send({ error });
+    // Check if the message exceeds the limit of 1000 characters
+    if (message.length > 1000) {
+      return res
+        .status(400)
+        .send({ error: "Message exceeds the character limit of 1000." });
     }
 
-    if (!JWT_SECRET) {
-      const error = "Please provide JWT secret in config (.env) files";
-      return res.status(500).send({ error });
-    }
+    // extracting user_email from req object that was added in Middleware
+    const user_email = (req as Request & { email?: string }).email;
 
-    const token = authorization.split(" ")[1];
-
-    //TODO: better to make a middleware & adding user_id in additional req headers
-    // break and return 402 because of lack of authorized token
-    if (!token) {
-      return res.status(402).send({ error: "No JWT Token Found" });
-    }
-
-    // verify iwt token
-    const user = jwt.verify(token, JWT_SECRET) as JwtPayload;
-    const user_email = user.email;
-
+    // extracting user info from database users
     const userInfo: QueryResult = await pool.query(
       "SELECT * FROM users WHERE user_email = $1",
       [user_email],
     );
+    // filtering userInfo to get user_id
     const user_id = userInfo.rows[0].user_id;
 
     // Trim whitespac from inputs
@@ -48,6 +33,7 @@ const createThread: RequestHandler = async (req: Request, res: Response) => {
     // wait for table to be created
     await creatTable();
 
+    // inserting thread data into table threads
     await pool.query(
       "INSERT INTO threads (thread_title, thread_message, leader_user_id) VALUES ($1, $2, $3)",
       [trimmedTitle, trimmedMessage, user_id],
@@ -56,6 +42,7 @@ const createThread: RequestHandler = async (req: Request, res: Response) => {
     // Thread Created successful
     return res.sendStatus(200);
   } catch (error) {
+    // Handle errors
     console.error("Error creating thread:", error);
     return res.status(500).send({ error: "Internal server error." });
   }
