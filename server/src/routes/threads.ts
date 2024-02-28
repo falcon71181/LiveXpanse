3a5f9ef14ad7ede8bef9f261c6ce513a103dc6ea
falcon71181 <abhaythakur71181@gmail.com>
2024-02-28 17:32:47 +0530

    Tag a Reply Added
    Added table for storing tags to replies of threads + changed names of some functions

--- server/src/routes/threads.ts
+++ server/src/routes/threads.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createThread } from "../controllers/thread.controller";
-import { createReply } from "../controllers/reply.controller";
+import { createReply, createSubReply } from "../controllers/reply.controller";
 import { isAuth } from "../middlewares/authReq";
 import type { IRouter } from "express";
 
@@ -9,7 +9,10 @@ const router: IRouter = Router();
 // threads/create
 router.post("/create", isAuth, createThread);
 
-// threads/reply/:thread_Id
-router.post("/reply/:threadId", isAuth, createReply);
+// threads/:thread_Id/replies
+router.post("/:threadId/replies", isAuth, createReply);
+
+// threads/:thread_Id/replies/:parentId/replies
+router.post("/:threadId/replies/:parentId/replies", isAuth, createSubReply);
 
 export { router as thread_routes };
