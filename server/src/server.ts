5231ff49c16ee8142ce69809e9b57c4980dd7cdf
Dev <flashytesting@gmail.com>
2024-03-02 17:10:45 +0530

    Add client prod origin for socket conection

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,7 +24,7 @@ const io = new Server(httpServer, {
       `http://localhost:${CLIENT_PORT}`,
       "http://localhost:3000",
       "http://localhost:8000",
-      "http://localhost:5173",
+      "http://localhost:4173",
     ],
   },
 });
