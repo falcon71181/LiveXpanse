6e77bbf72ebf273ca64baee4b192eb646d637089
Dev <flashytesting@gmail.com>
2024-03-07 19:42:59 +0530

    Refactor according to new file structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,32 @@
-import * as dotenv from "dotenv";
-// to read DotEnv file variables
-dotenv.config();
-
+// Core packages
 import cors from "cors";
 import express from "express";
-import { user_routes } from "./routes/users";
-import { thread_routes } from "./routes/threads";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import { getOrigins } from "./utils/origin";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-// port for server
-const SERVER_PORT = process.env.SERVER_PORT || 3333;
-const ORIGINS = getOrigins()
+// Others (routes/utils)
+import { user_routes } from "./routes/users";
+import { thread_routes } from "./routes/threads";
+import { getOrigins, getMethods } from "./lib/utils";
 
+const SERVER_PORT = process.env.SERVER_PORT;
+const ORIGINS = getOrigins();
+const METHODS = getMethods();
+
+// Express server
 const app = express();
 const httpServer = createServer(app);
 
-// Create an instance of the Socket.IO server
 const io = new Server(httpServer, {
   cors: {
     origin: ORIGINS,
   },
 });
 
-// CORS Options
-const corsOptions = {
-  origin: ORIGINS,
-  methods: (process.env?.ALLOWED_METHODS || "GET", "POST").split(","),
-};
-
 // Middlewares
-app.use(cors(corsOptions));
+app.use(cors({ origin: ORIGINS, methods: METHODS }));
 app.use(express.json());
 
 // Health Check API Route
@@ -39,29 +34,24 @@ app.get("/health", (_req, res) => {
   res.sendStatus(200);
 });
 
-// User Login & Register routes
+// Other routes
 app.use("/users", user_routes);
-
-// Thread create routes
 app.use("/threads", thread_routes);
 
-// Socket.IO event handlers
 io.on("connection", (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
+  console.log(`🟢 ${socket.id} CONNECTED!`);
 
   socket.on("sent-message", (messageData) => {
     io.emit("received-message", messageData);
   });
 
   socket.on("disconnect", () => {
-    console.log(`🔥: ${socket.id} user just disconnected`);
+    console.log(`🔴 ${socket.id} DISCONNECTED`);
   });
 });
 
-// Starting Server with SERVER_PORT
 httpServer.listen(SERVER_PORT, () => {
-  console.log(`Server is running on : ${SERVER_PORT}`);
+  console.log(`Server is listening on port ${SERVER_PORT}`);
 });
 
-// Exporting app for Vercel API
 export default app;
