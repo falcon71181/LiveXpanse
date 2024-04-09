c0ded3aebdd572613a8aa1bf5b9d3288cfc47428
Dev <flashytesting@gmail.com>
2024-04-09 20:10:06 +0530

    Fix text selection bug on blured background on active thread form

--- client/src/pages/Forum.tsx
+++ client/src/pages/Forum.tsx
@@ -37,14 +37,14 @@ const Forum = () => {
 
   return (
     <div>
-      <div className={`${threadPop ? 'blur-md pointer-events-none' : 'blur-none'} relative pt-16 w-screen flex justify-center min-h-screen text-zinc-300 transition-all duration-100`}>
+      <div className={`${threadPop ? 'blur-md pointer-events-none select-none' : 'blur-none'} relative pt-16 w-screen flex justify-center min-h-screen text-zinc-300 transition-all duration-100`}>
         <div className="m-1 sm:m-3 md:m-6 w-full sm:w-10/12 md:w-9/12 lg:w-8/12">
           <div className='relative max-w-2xl mx-auto p-2'>
             <h1 className="w-full flex justify-center text-4xl text-white font-bold tracking-wider overflow-hidden">
               LiveXpanse Connect
             </h1>
             {authUser && (
-              <button onClick={() => setThreadPop(!threadPop)} className='absolute top-1/2 right-0 -translate-y-1/3 px-3 py-2.5 inline-flex justify-center items-center text-sm rounded-md bg-[#34495E] border border-gray-500 cursor-pointer hover:bg-[#2C3E50]'>
+              <button onClick={() => setThreadPop(!threadPop)} className='absolute top-1/2 right-0 -translate-y-1/2 px-3 py-2.5 inline-flex justify-center items-center text-sm rounded-md bg-[#34495E] border border-gray-500 cursor-pointer hover:bg-[#2C3E50]'>
                 New Thread
               </button>
             )}
