51839a6dd58be3ae9fe02dbfe9165a839e588b1d
Dev <flashytesting@gmail.com>
2024-04-22 18:46:10 +0530

    refactor: change all timestamp usage to a formated time

--- client/src/components/Forum/Post/PostCard.tsx
+++ client/src/components/Forum/Post/PostCard.tsx
@@ -1,32 +1,17 @@
-import { useState, useEffect } from "react"
 import { FaReply } from "react-icons/fa";
 import { PostInfo } from "../../../types/posts";
+import { formateTime } from "../../../lib/utils";
 
 interface PostCardProps {
   props: PostInfo;
 }
 
 export const PostCard: React.FC<PostCardProps> = ({ props: postData }) => {
-  const [postCreatedOn, setPostCreatedOn] = useState("");
-
-  useEffect(() => {
-    {
-      /* TODO: Make it in ago format */
-    }
-    const fetchTime = async () => {
-      const timestamp = parseInt(postData.createdOn);
-      const date = new Date(timestamp);
-      const messageDateTime = date.toLocaleTimeString();
-      setPostCreatedOn(messageDateTime);
-    };
-    fetchTime();
-  }, [postData.createdOn]);
-
   return (
     <main className="relative min-h-60 p-3 md:p-5 lg:p-6 rounded-xl bg-background_card flex flex-col overflow-hidden border-[1px] border-[#2fa8a3] hover:border-[#47FFF7]">
       <section className="text-sm md:text-base h-10 flex justify-between items-center">
         <h1 className="text-[#a970ff]">by <span className="text-base inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#2fa8a3] to-[#8678f9] bg-[200%_auto] bg-clip-text text-transparent font-semibold text-ellipsis overflow-hidden tracking-wide">{postData.leader}</span></h1>
-        <span className="text-zinc-300">{postCreatedOn}</span>
+        <span className="text-zinc-300">{formateTime(postData.createdOn)}</span>
       </section>
       <div className="min-h-10 font-semibold text-2xl bg-gradient-to-t from-[#c7d2fe] to-[#8678f9] bg-clip-text text-transparent text-ellipsis overflow-hidden whitespace-nowrap">
         {postData.title}
