342221f323e1d1317be9854418c6b9b1a0f9a1c0
Dev <flashytesting@gmail.com>
2024-05-22 11:33:52 +0530

    style: remove searchbar (unused for the moment)

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -42,6 +42,29 @@ const NavBar = () => {
     };
   }, []);
 
+  // <section className="flex items-center justify-center h-8">
+  //   {/* Make Search Bar Workable */}
+  //   <input
+  //     type="text"
+  //     name="text"
+  //     className="input text-[#f7f7f8] mr-[1px] p-[10px] outline-none w-72 h-full bg-[#151E27] transition-all duration-200 ease rounded-l border-[1px] border-white focus:border-[2px] focus:border-[#a970ff] focus:bg-[#0e0e10]"
+  //     placeholder="Search"
+  //   ></input>
+  //   <button className="h-full bg-gray-800 rounded-r-lg flex items-center justify-center w-10 cursor-pointer hover:bg-gray-50">
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       viewBox="0 0 24 24"
+  //       width="24"
+  //       height="22"
+  //     >
+  //       <path
+  //         d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"
+  //         fill="#efeff1"
+  //       ></path>
+  //     </svg>
+  //   </button>
+  // </section>
+
   return (
     <main className="px-8 py-3 bg-[#151E27] fixed w-full flex items-center justify-between z-50 border-b border-gray-600">
       <section className="flex items-center gap-3 z-50">
@@ -84,28 +107,6 @@ const NavBar = () => {
           </div>
         </Link>
         {/* Add More Nav Options here */}
-      </section>
-      <section className="flex items-center justify-center h-8">
-        {/* Make Search Bar Workable */}
-        <input
-          type="text"
-          name="text"
-          className="input text-[#f7f7f8] mr-[1px] p-[10px] outline-none w-72 h-full bg-[#151E27] transition-all duration-200 ease rounded-l border-[1px] border-white focus:border-[2px] focus:border-[#a970ff] focus:bg-[#0e0e10]"
-          placeholder="Search"
-        ></input>
-        <button className="h-full bg-gray-800 rounded-r-lg flex items-center justify-center w-10 cursor-pointer hover:bg-gray-50">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="22"
-          >
-            <path
-              d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"
-              fill="#efeff1"
-            ></path>
-          </svg>
-        </button>
       </section>
       {authUser ? (
         <div className='flex gap-4'>
